d440c11e0c02674d0edf6728406bb3d13ac9ab26
Anand K H <khanand1998@gmail.com>
2023-11-04 00:10:34 +0530

    added healthy, exchange and failed battery count based on SoH

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
-const assert = require('assert');
+const assert = require("assert");
 
 function countBatteriesByHealth(presentCapacities) {
+  var data = [];
+
+  //calculating SoH and pushing it into an array and rounding off to 2 decimal places
+  presentCapacities?.forEach((ele) => {
+    data.push(Number(100 * (ele / 120)).toFixed(2));
+  });
+
+  //taking variables and counting the healthy, exchange and failed batteries based on SoH
+  var healthbattery = 0,
+    exchangebattery = 0,
+    failedbattery = 0;
+  data.forEach((ele) => {
+    if (Number(ele) > 80 && Number(ele) <= 100) {
+      healthbattery += 1;
+    } else if (Number(ele) < 80 && Number(ele) > 62) {
+      exchangebattery += 1;
+    } else {
+      failedbattery += 1;
+    }
+  });
+
   return {
-    healthy: 0,
-    exchange: 0,
-    failed: 0
+    healthy: healthbattery,
+    exchange: exchangebattery,
+    failed: failedbattery,
   };
 }
 
 function testBucketingByHealth() {
-  console.log('Counting batteries by SoH...');
+  console.log("Counting batteries by SoH...");
   const presentCapacities = [113, 116, 80, 95, 92, 70];
   counts = countBatteriesByHealth(presentCapacities);
   assert(counts["healthy"] == 2);
